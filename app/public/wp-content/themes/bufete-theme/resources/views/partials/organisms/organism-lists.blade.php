d3d72713ebe0facdef7b0132a9a8f72dd184ae56
Adolfo Saenz <adolfo.saenz@globant.com>
2025-06-12 13:54:07 +0200

    Modified the width of the bar near to the title

--- app/public/wp-content/themes/bufete-theme/resources/views/partials/organisms/organism-lists.blade.php
+++ app/public/wp-content/themes/bufete-theme/resources/views/partials/organisms/organism-lists.blade.php
@@ -15,7 +15,7 @@
     <section>
         <div class="flex items-center space-x-2 mt-5 mb-5">
             <h3 class="font-bold text-[45px] ml-8">Casos</h3>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
             </svg>  
         </div>
