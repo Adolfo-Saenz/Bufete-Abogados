7a9cb6f80ddf0e7ff7e54545d16e7ecfbf2edeea
Adolfo Saenz <adolfo.saenz@globant.com>
2025-06-12 09:23:30 +0200

    Added the bills section to the organism

--- app/public/wp-content/cache/acorn/framework/views/881c0ebc14ec5bd200ceae55b7c41950.php
+++ app/public/wp-content/cache/acorn/framework/views/881c0ebc14ec5bd200ceae55b7c41950.php
@@ -1,27 +1,28 @@
 
 <?php
+    $user = wp_get_current_user();
+    $user_id = $user->ID;
+
     /* Obtenemos las categorías de Wordpress */
     $categorias = get_categories([
-        'hide_empty' => false, // mostrar también las vacías
+        'hide_empty' => false, // mostrar las vacías
         'number' => 4,
     ]);
 
     $args = [
         'post_type' => 'caso',
-        'posts_per_page' => 4,
     ];
-
     $query = new WP_Query($args);
-
-    $user = wp_get_current_user();
-    $user_id = $user->ID;
 
     $argsEventos = [
         'post_type' => 'evento',
-        'posts_per_page' => 4,
     ];
-    
     $eventos = new WP_Query($argsEventos);
+
+    $argsBills = [
+        'post_type' => 'factura',
+    ];
+    $bills = new WP_Query($argsBills);
 ?>
 
 <?php if($tipo == "Espec"): ?>
@@ -88,25 +89,27 @@
                     $lawyer_user = get_userdata($lawyer_id);
                     $lawyer_name = $lawyer_user ? $lawyer_user->display_name : 'Abogado desconocido';
                 ?>
-                <?php if($client_id == $user_id || $lawyer_id == $user_id): ?>
-                    <?php
-                        $cuenta++;
-                    ?>
-                    <div class="h-responsive bg-gray-200 border-0 rounded-[15px] flex flex-col justify-center gap-2 mb-6">
-                        <div class="m-4 flex justify-center">
-                            <img src="<?php echo e($image_url); ?>" alt="Imagen tipo caso" class="h-[150px] border-0 rounded-[10px] object-cover">
+                <?php if($cuenta < 4): ?>
+                    <?php if($client_id == $user_id || $lawyer_id == $user_id): ?>
+                        <?php
+                            $cuenta++;
+                        ?>
+                        <div class="h-responsive bg-gray-200 border-0 rounded-[15px] flex flex-col justify-center gap-2 mb-6">
+                            <div class="m-4 flex justify-center">
+                                <img src="<?php echo e($image_url); ?>" alt="Imagen tipo caso" class="h-[150px] border-0 rounded-[10px] object-cover">
+                            </div>
+                            <div class="mr-4 ml-4">
+                                <h3 class="text-[18px] font-bold mb-1"><?php echo e($caso->post_title); ?></h3>
+                                <p><strong>Cliente:</strong> <?php echo e($client_name); ?></p>
+                                <p><strong>Abogado(s):</strong> <?php echo e($lawyer_name); ?></p>
+                            </div>
+                            <div class="m-4">
+                                <a href="<?php echo e($caso->guid); ?>" class="inline-block hover:bg-[#767CB5] bg-[#6A6B75] text-white px-4 py-2 rounded transition">
+                                    Leer caso
+                                </a>
+                            </div>
                         </div>
-                        <div class="mr-4 ml-4">
-                            <h3 class="text-[18px] font-bold mb-1"><?php echo e($caso->post_title); ?></h3>
-                            <p><strong>Cliente:</strong> <?php echo e($client_name); ?></p>
-                            <p><strong>Abogado(s):</strong> <?php echo e($lawyer_name); ?></p>
-                        </div>
-                        <div class="m-4">
-                            <a href="<?php echo e($caso->guid); ?>" class="inline-block hover:bg-[#767CB5] bg-[#6A6B75] text-white px-4 py-2 rounded transition">
-                                Leer caso
-                            </a>
-                        </div>
-                    </div>
+                    <?php endif; ?>
                 <?php endif; ?>
             <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
             <?php if($cuenta == 0): ?> 
@@ -116,7 +119,7 @@
             <?php endif; ?>
         </div>
     </section>
-<?php else: ?>
+<?php elseif($tipo == "Event"): ?>
     
     <section class="px-6 py-10">
         <div class="grid grid-cols-1 gap-2 lg:grid-cols-[1fr_auto] items-center mb-3">
@@ -151,32 +154,125 @@
                     $lawyer_user_ev = get_userdata($lawyer_id_ev);
                     $lawyer_name_ev = $lawyer_user_ev ? $lawyer_user_ev->display_name : 'Abogado desconocido';
                 ?>
-                <?php if($client_id_ev == $user_id || $lawyer_id_ev == $user_id): ?>
-                    <?php
-                        $cuenta++;
-                    ?>
-                    <div class="h-responsive bg-gray-200 border-0 rounded-[15px] flex flex-col justify-center gap-2 mb-6">
-                        <div class="m-4 flex justify-center">
-                            <img src="<?php echo e($image_url); ?>" alt="Imagen tipo caso" class="h-[150px] border-0 rounded-[10px] object-cover">
+                <?php if($cuenta < 4): ?>
+                    <?php if($client_id_ev == $user_id || $lawyer_id_ev == $user_id): ?>
+                        <?php
+                            $cuenta++;
+                        ?>
+                        <div class="h-responsive bg-gray-200 border-0 rounded-[15px] flex flex-col justify-center gap-2 mb-6">
+                            <div class="m-4 flex justify-center">
+                                <img src="<?php echo e($image_url); ?>" alt="Imagen tipo caso" class="h-[150px] border-0 rounded-[10px] object-cover">
+                            </div>
+                            <div class="mr-4 ml-4">
+                                <h3 class="text-[18px] font-bold mb-1"><?php echo e($evento->post_title); ?></h3>
+                                <p><strong>Cliente:</strong> <?php echo e($client_name_ev); ?></p>
+                                <p><strong>Abogado:</strong> <?php echo e($lawyer_name_ev); ?></p>
+                                <p><strong>Comienza:</strong> <?php echo e($fecha_start); ?></p>
+                                <p><strong>Finaliza:</strong> <?php echo e($fecha_end); ?></p>
+                            </div>
+                            <div class="m-4">
+                                <a href="<?php echo e($evento->guid); ?>" class="inline-block hover:bg-[#767CB5] bg-[#6A6B75] text-white px-4 py-2 rounded transition">
+                                    Ver evento
+                                </a>
+                            </div>
                         </div>
-                        <div class="mr-4 ml-4">
-                            <h3 class="text-[18px] font-bold mb-1"><?php echo e($evento->post_title); ?></h3>
-                            <p><strong>Cliente:</strong> <?php echo e($client_name_ev); ?></p>
-                            <p><strong>Abogado:</strong> <?php echo e($lawyer_name_ev); ?></p>
-                            <p><strong>Comienza:</strong> <?php echo e($fecha_start); ?></p>
-                            <p><strong>Finaliza:</strong> <?php echo e($fecha_end); ?></p>
-                        </div>
-                        <div class="m-4">
-                            <a href="<?php echo e($evento->guid); ?>" class="inline-block hover:bg-[#767CB5] bg-[#6A6B75] text-white px-4 py-2 rounded transition">
-                                Ver evento
-                            </a>
-                        </div>
-                    </div>
-                <?php endif; ?> 
+                    <?php endif; ?> 
+                <?php endif; ?>
             <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
             <?php if($cuenta == 0): ?> 
                 <div class="h-responsive border-0 rounded-[15px] flex flex-col justify-center gap-2 mb-6">
                     No tienes ningún evento
+                </div>  
+            <?php endif; ?>
+        </div>
+    </section>
+<?php elseif($tipo == "Bills"): ?>
+    
+    <section class="px-6 py-10">
+        <div class="grid grid-cols-1 gap-2 lg:grid-cols-[1fr_auto] items-center mb-3">
+            <div class="flex items-center space-x-2">
+                <h3 class="font-bold text-[25px]">Facturas</h3>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
+                </svg>  
+            </div>
+            <a href="#" class="text-[16px] w-[70px] h-[25px]">Ver todas</a>
+        </div>
+        <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            <?php
+                $cuenta = 0;
+            ?>
+            <?php $__currentLoopData = $bills->posts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $bill): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <?php
+                    $evento_relacionado = get_field('evento', $bill->ID);
+                    $evento_relacionado = $evento_relacionado[0];
+                    $caso_relacionado = get_field('caso-relacionado', $evento_relacionado->ID);
+                    $caso_relacionado = $caso_relacionado[0];
+
+                    $tipo_caso_id = get_field('type', $caso_relacionado->ID);
+                    $image_url = $tipo_caso_id ? get_field('category-pic', 'term_' . $tipo_caso_id) : null;
+
+                    $client_id_ev = get_field('client', $caso_relacionado->ID);
+                    $client_user_ev = get_userdata($client_id_ev);
+                    $client_name_ev = $client_user_ev ? $client_user_ev->display_name : 'Cliente desconocido';
+
+                    $lawyer_id_ev = get_field('lawyer', $caso_relacionado->ID);
+                    $lawyer_user_ev = get_userdata($lawyer_id_ev);
+                    $lawyer_name_ev = $lawyer_user_ev ? $lawyer_user_ev->display_name : 'Abogado desconocido';
+
+                    $fecha_inicio_o = get_field('fecha-inicio', $evento_relacionado->ID);
+                    $fecha_fin_o = get_field('fecha-fin', $evento_relacionado->ID);
+                    $precio_hora = get_field('precio-hora', $bill->ID);
+
+                    $formato = 'd/m/Y g:i a';
+                    $fecha_inicio = DateTime::createFromFormat($formato, $fecha_inicio_o);
+                    $fecha_fin = DateTime::createFromFormat($formato, $fecha_fin_o);
+                    $intervalo = $fecha_inicio->diff($fecha_fin);
+                    $horas = $intervalo->h + ($intervalo->days * 24); // incluye días convertidos a horas
+                    $minutos = $intervalo->i;
+                    $horas_totales_decimal = $horas + ($minutos / 60);
+                    $total_pagar = $horas_totales_decimal * $precio_hora;
+                ?>
+                <?php if($cuenta < 4): ?>
+                    
+                        <?php
+                            $cuenta++;
+                        ?>
+                        <div class="h-responsive bg-gray-200 border-0 rounded-[15px] flex flex-col justify-center gap-2 mb-6">
+                            <div class="m-4 flex justify-center">
+                                <img src="<?php echo e($image_url); ?>" alt="Imagen tipo caso" class="h-[150px] border-0 rounded-[10px] object-cover">
+                            </div>
+                            <div class="mr-4 ml-4">
+                                <h3 class="text-[18px] font-bold mb-1"><?php echo e($bill->post_title); ?></h3>
+                                <p><strong>Evento:</strong> <?php echo e($evento_relacionado->post_title); ?></p>
+                                <p><strong>Abogado:</strong> <?php echo e($lawyer_name_ev); ?></p>
+                                <?php if($horas > 1): ?>
+                                    <?php if($minutos == ""): ?>
+                                        <p><strong>Tiempo:</strong> <?php echo e($horas); ?> Horas</p>
+                                    <?php else: ?>
+                                        <p><strong>Tiempo:</strong> <?php echo e($horas); ?> Horas y <?php echo e($minutos); ?> minutos</p>
+                                    <?php endif; ?>
+                                <?php else: ?>
+                                    <?php if($minutos == ""): ?>
+                                        <p><strong>Tiempo:</strong> <?php echo e($horas); ?> Hora</p>
+                                    <?php else: ?>
+                                        <p><strong>Tiempo:</strong> <?php echo e($horas); ?> Hora y <?php echo e($minutos); ?> minutos</p>
+                                    <?php endif; ?>
+                                <?php endif; ?>
+                                <p><strong>Total a pagar:</strong> <?php echo e($total_pagar); ?> €</p>
+                            </div>
+                            <div class="m-4">
+                                <a href="<?php echo e($bill->guid); ?>" class="inline-block hover:bg-[#767CB5] bg-[#6A6B75] text-white px-4 py-2 rounded transition">
+                                    Ver factura
+                                </a>
+                            </div>
+                        </div>
+                     
+                <?php endif; ?>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+            <?php if($cuenta == 0): ?> 
+                <div class="h-responsive border-0 rounded-[15px] flex flex-col justify-center gap-2 mb-6">
+                    No tienes ninguna factura
                 </div>  
             <?php endif; ?>
         </div>
