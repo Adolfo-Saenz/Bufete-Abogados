06696fbc770ccf95ef65c358f05c175b870079d6
Adolfo Saenz <adolfo.saenz@globant.com>
2025-06-12 12:54:37 +0200

    Full created the cases page list

--- app/public/wp-content/cache/acorn/framework/views/c69477886554be6e81076dbea840c8af.php
+++ app/public/wp-content/cache/acorn/framework/views/c69477886554be6e81076dbea840c8af.php
@@ -1,10 +1,8 @@
-{{--
-  Template Name: Cases Template
---}}
 
-@extends('layouts.app')
 
-@php
+
+
+<?php
     $user = wp_get_current_user();
     $user_id = $user->ID;
     $perfil = $user->roles;
@@ -13,23 +11,23 @@
         'post_type' => 'caso',
     ];
     $query = new WP_Query($args);
-@endphp
+?>
 
-{{-- Header --}}
-@include('partials.organisms.organism-header')
 
-{{-- Cases --}}
+<?php echo $__env->make('partials.organisms.organism-header', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
+
+
 <div class="flex items-center space-x-2 mt-5 mb-5">
     <h3 class="font-bold text-[45px] ml-8">Casos</h3>
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
     </svg>  
 </div>
-@php
+<?php
     $cuenta = 0;
-@endphp
-@foreach ($query->posts as $caso)
-    @php
+?>
+<?php $__currentLoopData = $query->posts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $caso): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <?php
         $tipo_caso_id = get_field('type', $caso->ID);
         $image_url = $tipo_caso_id ? get_field('category-pic', 'term_' . $tipo_caso_id) : null;
 
@@ -44,56 +42,58 @@
         $lawyer_image = get_field('profile-image', 'user_' . $lawyer_id);
 
         $desc = get_field('desc', $caso->ID);
-    @endphp
-    @if($client_id == $user_id || $lawyer_id == $user_id)
-        @php
+    ?>
+    <?php if($client_id == $user_id || $lawyer_id == $user_id): ?>
+        <?php
             $cuenta++;
-        @endphp
+        ?>
         <div class="py-5 px-10">
             <div class="mb-7 grid grid-cols-5 gap-4">
                 <div class="col-span-1 flex items-center justify-center">
-                    <img src="{{ $image_url }}" alt="Imagen tipo caso" class="h-[150px] border-0 rounded-[10px] object-cover">
+                    <img src="<?php echo e($image_url); ?>" alt="Imagen tipo caso" class="h-[150px] border-0 rounded-[10px] object-cover">
                 </div>
                 <div class="col-span-4">
-                    {{-- Título del caso --}}
-                    <h1 class="mb-2 text-[30px] font-bold">{{ $caso->post_title }}</h1>
-                    {{-- Abogado y cliente --}}
+                    
+                    <h1 class="mb-2 text-[30px] font-bold"><?php echo e($caso->post_title); ?></h1>
+                    
                     <div class="flex flex-row items-center gap-4 mb-3">
-                        @if($lawyer_image != "")
-                            <img src="{{ $lawyer_image }}" alt="Imagen Juez" class="w-[40px] h-[40px] rounded-full">
-                        @else
+                        <?php if($lawyer_image != ""): ?>
+                            <img src="<?php echo e($lawyer_image); ?>" alt="Imagen Juez" class="w-[40px] h-[40px] rounded-full">
+                        <?php else: ?>
                             <img src="http://bufete-abogados.local/wp-content/uploads/2025/06/default.png" alt="Imagen Juez" class="w-[40px] h-[40px] rounded-full">
-                        @endif
-                        <p>{{ $lawyer_name }}</p>
+                        <?php endif; ?>
+                        <p><?php echo e($lawyer_name); ?></p>
                     </div>
                     <div class="flex flex-row items-center gap-4 mb-3">
-                        @if($client_image != "")
-                            <img src="{{ $client_image }}" alt="Imagen cliente" class="w-[40px] h-[40px] rounded-full">
-                        @else
+                        <?php if($client_image != ""): ?>
+                            <img src="<?php echo e($client_image); ?>" alt="Imagen cliente" class="w-[40px] h-[40px] rounded-full">
+                        <?php else: ?>
                             <img src="http://bufete-abogados.local/wp-content/uploads/2025/06/default.png" alt="Imagen cliente" class="w-[40px] h-[40px] rounded-full">
-                        @endif
-                        <p>{{ $client_name }}</p>
+                        <?php endif; ?>
+                        <p><?php echo e($client_name); ?></p>
                     </div>
-                    {{-- Contenido del post --}}
+                    
                     <p class="text-[15px] mt-4 mb-4">
-                        {{ wp_trim_words($desc, 30, '...') }}
+                        <?php echo e(wp_trim_words($desc, 30, '...')); ?>
+
                     </p>
-                    {{-- Botón --}}
+                    
                     <div>
-                        <a href="{{ $caso->guid }}" class="inline-block hover:bg-[#767CB5] bg-[#6A6B75] text-white px-4 py-2 rounded transition">
+                        <a href="<?php echo e($caso->guid); ?>" class="inline-block hover:bg-[#767CB5] bg-[#6A6B75] text-white px-4 py-2 rounded transition">
                             Leer caso
                         </a>
                     </div>
                 </div>
             </div>
         </div>
-    @endif
-@endforeach
-@if ($cuenta == 0) 
+    <?php endif; ?>
+<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+<?php if($cuenta == 0): ?> 
     <div class="h-responsive border-0 rounded-[15px] flex flex-col justify-center gap-2 mb-6 ml-6">
         No perteneces a ningún caso
     </div>  
-@endif
+<?php endif; ?>
 
-{{-- Footer/Pie de página --}}
-@include('partials.organisms.organism-footer')
+
+<?php echo $__env->make('partials.organisms.organism-footer', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
+<?php echo $__env->make('layouts.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\Users\adolfo.saenz\Local Sites\bufete-abogados\app\public\wp-content\themes\bufete-theme\resources\views/page-cases-template.blade.php ENDPATH**/ ?>
