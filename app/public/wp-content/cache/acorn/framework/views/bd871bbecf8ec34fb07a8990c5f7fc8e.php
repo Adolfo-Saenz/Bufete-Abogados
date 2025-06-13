6e0da52d791f179f769d4282edc5d2ebf2fc0808
Adolfo Saenz <adolfo.saenz@globant.com>
2025-06-13 12:43:54 +0200

    Added the see more button on the events, and a grid where you can see the last 2 events of the case. In the future you would slide the events and see all of them

--- app/public/wp-content/cache/acorn/framework/views/bd871bbecf8ec34fb07a8990c5f7fc8e.php
+++ app/public/wp-content/cache/acorn/framework/views/bd871bbecf8ec34fb07a8990c5f7fc8e.php
@@ -2,6 +2,10 @@
 
 
 <?php
+    $user = wp_get_current_user();
+    $user_id = $user->ID;
+    $perfil = $user->roles;
+
     $tipo_caso_id = get_field('type');
     $image_url = $tipo_caso_id ? get_field('category-pic', 'term_' . $tipo_caso_id) : null;
 
@@ -18,6 +22,11 @@
     $fecha_inicio = get_field('starting-date');
 
     $desc = get_field('desc');
+
+    $argsEventos = [
+        'post_type' => 'evento',
+    ];
+    $eventos = new WP_Query($argsEventos);
 ?>
 
 <head>
@@ -31,30 +40,107 @@
 
 <?php echo $__env->make('partials.organisms.organism-header', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
 
-
-<section class="px-10 py-10 max-md:p-8 flex flex-row items-justified-space-between max-md:flex-col">
-    <div class="lg:w-2/5 max-lg:3/5 max-md:w-full flex flex-col gap-5 max-md:mb-6">
-        <h1 class="text-[45px] font-bold mb-5"><?php echo e(get_the_title()); ?></h1>
-        <p class="text-[20px]"><b>Cliente:</b> <?php echo e($client_name); ?></p>
-        <p class="text-[20px]"><b>Abogado:</b> <?php echo e($lawyer_name); ?></p>
-        <p class="text-[20px]"><b>Estado:</b> <?php echo e($estado); ?></p>
-        <p class="text-[20px]"><b>Fecha de inicio:</b> <?php echo e($fecha_inicio); ?></p>
-    </div>
-    <div class="max-lg:hidden lg:w-1/5 flex items-center justify-center">
-        <img src="<?php echo e($image_url); ?>" alt="Imagen tipo de caso">
-    </div>
-    <div class="max-md:w-full h-responsive w-2/5 bg-cover flex items-center justify-center max-md:mb-10">
-        <!-- my calendar -->
-        <div class="auto-jsCalendar" data-month-format="month YYYY"  data-day-format="DDD" data-language="es" data-first-day-of-the-week="2">
+<?php if($client_id == $user_id || $lawyer_id == $user_id || $perfil[0] == 'administrator'): ?>
+    
+    <section class="px-10 py-10 max-md:p-8 flex flex-row items-justified-space-between max-md:flex-col">
+        <div class="lg:w-2/5 max-lg:3/5 max-md:w-full flex flex-col gap-5 max-md:mb-6">
+            <h1 class="text-[45px] font-bold mb-5"><?php echo e(get_the_title()); ?></h1>
+            <p class="text-[20px]"><b>Cliente:</b> <?php echo e($client_name); ?></p>
+            <p class="text-[20px]"><b>Abogado:</b> <?php echo e($lawyer_name); ?></p>
+            <p class="text-[20px]"><b>Estado:</b> <?php echo e($estado); ?></p>
+            <p class="text-[20px]"><b>Fecha de inicio:</b> <?php echo e($fecha_inicio); ?></p>
         </div>
-    </div>
-</section>
+        <div class="max-lg:hidden lg:w-1/5 flex items-center justify-center">
+            <img src="<?php echo e($image_url); ?>" alt="Imagen tipo de caso">
+        </div>
+        <div class="max-md:w-full h-responsive w-2/5 bg-cover flex items-center justify-center max-md:mb-10">
+            <!-- my calendar -->
+            <div class="auto-jsCalendar" data-month-format="month YYYY"  data-day-format="DDD" data-language="es" data-first-day-of-the-week="2">
+            </div>
+        </div>
+    </section>
+
+    
+    <section class="px-10 py-10">
+        <h1 class="text-[40px] font-bold mb-8">Descripcion:</h1>
+        <p class="text-[20px]"><?php echo nl2br(e($desc)); ?></p>
+    </section>
+
+    
+    <section class="px-10 py-10">
+        <div class="flex flex-row max-sm:flex-col items-center items-justified-space-between">
+            <h1 class="text-[40px] font-bold mb-8">Eventos:</h1>
+            <a href="#">Ver más</a>
+        </div>
+        <div class="flex md:flex-row items-justified-space-between">
+            <div class="max-md:w-full h-responsive w-2/5 bg-cover flex items-center justify-center max-md:mb-10">
+                <!-- my calendar -->
+                <div class="auto-jsCalendar" data-month-format="month YYYY"  data-day-format="DDD" data-language="es" data-first-day-of-the-week="2">
+                </div>
+            </div>
+            <div class="grid sm:grid-cols-2 gap-10">
+                <?php
+                    $cuenta = 0;
+                ?>
+                <?php $__currentLoopData = $eventos->posts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $evento): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <?php
+                        $fecha_start = get_field('fecha-inicio', $evento->ID);
+                        $fecha_end = get_field('fecha-fin', $evento->ID);
+
+                        $caso_relacionado = get_field('caso-relacionado', $evento->ID);
+                        $caso_relacionado = $caso_relacionado[0];
+
+                        $tipo_caso_id = get_field('type', $caso_relacionado->ID);
+                        $image_url = $tipo_caso_id ? get_field('category-pic', 'term_' . $tipo_caso_id) : null;
+
+                        $client_id_ev = get_field('client', $caso_relacionado->ID);
+                        $client_user_ev = get_userdata($client_id_ev);
+                        $client_name_ev = $client_user_ev ? $client_user_ev->display_name : 'Cliente desconocido';
+
+                        $lawyer_id_ev = get_field('lawyer', $caso_relacionado->ID);
+                        $lawyer_user_ev = get_userdata($lawyer_id_ev);
+                        $lawyer_name_ev = $lawyer_user_ev ? $lawyer_user_ev->display_name : 'Abogado desconocido';
+                    ?>
+                    <?php if($cuenta < 2): ?>
+                        <?php if($caso_relacionado->ID == get_the_ID()): ?>
+                            <?php
+                                $cuenta++;
+                            ?>
+                            <div class="h-responsive bg-gray-200 border-0 rounded-[15px] flex flex-col justify-center gap-2 mb-6">
+                                <div class="m-4 flex justify-center">
+                                    <img src="<?php echo e($image_url); ?>" alt="Imagen tipo caso" class="h-[150px] border-0 rounded-[10px] object-cover">
+                                </div>
+                                <div class="mr-4 ml-4">
+                                    <h3 class="text-[18px] font-bold mb-1"><?php echo e($evento->post_title); ?></h3>
+                                    <p><strong>Cliente:</strong> <?php echo e($client_name_ev); ?></p>
+                                    <p><strong>Abogado:</strong> <?php echo e($lawyer_name_ev); ?></p>
+                                    <p><strong>Comienza:</strong> <?php echo e($fecha_start); ?></p>
+                                    <p><strong>Finaliza:</strong> <?php echo e($fecha_end); ?></p>
+                                </div>
+                                <div class="m-4">
+                                    <a href="<?php echo e(get_permalink($evento->ID)); ?>" class="inline-block hover:bg-[#767CB5] bg-[#6A6B75] text-white px-4 py-2 rounded transition">
+                                        Ver evento
+                                    </a>
+                                </div>
+                            </div>
+                        <?php endif; ?> 
+                    <?php endif; ?>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                <?php if($cuenta == 0): ?> 
+                    <div class="h-responsive border-0 rounded-[15px] flex flex-col justify-center gap-2 mb-6">
+                        No hay eventos relacionados a este caso
+                    </div>  
+                <?php endif; ?>
+            </div>
+        </div>
+    </section>
+<?php else: ?>
+    <section class="flex items-center justify-center text-[60px] font-bold">
+        No estás autorizado para ver este contenido
+    </section>
+<?php endif; ?>
 
 
-<section class="px-10 py-10">
-    <h1 class="text-[40px] font-bold mb-8">Descripcion:</h1>
-    <p class="text-[20px] mb-10"><?php echo nl2br(e($desc)); ?></p>
-</section>
 
 <?php echo $__env->make('partials.organisms.organism-footer', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
 <?php echo $__env->make('layouts.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\Users\adolfo.saenz\Local Sites\bufete-abogados\app\public\wp-content\themes\bufete-theme\resources\views/single-caso.blade.php ENDPATH**/ ?>
