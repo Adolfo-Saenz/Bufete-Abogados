3bb69fcb207dc98978983e470200f8a3de17affe
Adolfo Saenz <adolfo.saenz@globant.com>
2025-06-13 13:09:52 +0200

    Added the events lists so you can choose between the cases or the events

--- app/public/wp-content/cache/acorn/framework/views/c831ec03623a77803bc499d85599f04d.php
+++ app/public/wp-content/cache/acorn/framework/views/c831ec03623a77803bc499d85599f04d.php
@@ -9,6 +9,11 @@
         'post_type' => 'caso',
     ];
     $query = new WP_Query($args);
+
+    $argsEventos = [
+        'post_type' => 'evento',
+    ];
+    $eventos = new WP_Query($argsEventos);
 ?>
 
 <?php if($tipo == 'cases'): ?>
@@ -38,6 +43,87 @@
                 $lawyer_image = get_field('profile-image', 'user_' . $lawyer_id);
 
                 $desc = get_field('desc', $caso->ID);
+            ?>
+            <?php if($client_id == $user_id || $lawyer_id == $user_id ||(current_user_can('administrator'))): ?>
+                <?php
+                    $cuenta++;
+                ?>
+                <div class="border border-gray-400 py-5 px-10">
+                    <div class="mb-7 grid grid-cols-5 max-lg:grid-cols-4 gap-4">
+                        <div class="max-lg:col-span-4 col-span-1 flex items-center justify-center">
+                            <img src="<?php echo e($image_url); ?>" alt="Imagen tipo caso" class="h-[150px] border-0 rounded-[10px] object-cover">
+                        </div>
+                        <div class="col-span-4">
+                            
+                            <h1 class="mb-2 text-[30px] font-bold"><?php echo e($caso->post_title); ?></h1>
+                            
+                            <div class="flex flex-row items-center gap-4 mb-3">
+                                <?php if($lawyer_image != ""): ?>
+                                    <img src="<?php echo e($lawyer_image); ?>" alt="Imagen Juez" class="w-[40px] h-[40px] rounded-full">
+                                <?php else: ?>
+                                    <img src="http://bufete-abogados.local/wp-content/uploads/2025/06/default.png" alt="Imagen Juez" class="w-[40px] h-[40px] rounded-full">
+                                <?php endif; ?>
+                                <p><?php echo e($lawyer_name); ?></p>
+                            </div>
+                            <div class="flex flex-row items-center gap-4 mb-3">
+                                <?php if($client_image != ""): ?>
+                                    <img src="<?php echo e($client_image); ?>" alt="Imagen cliente" class="w-[40px] h-[40px] rounded-full">
+                                <?php else: ?>
+                                    <img src="http://bufete-abogados.local/wp-content/uploads/2025/06/default.png" alt="Imagen cliente" class="w-[40px] h-[40px] rounded-full">
+                                <?php endif; ?>
+                                <p><?php echo e($client_name); ?></p>
+                            </div>
+                            
+                            <p class="text-[15px] mt-4 mb-4">
+                                <?php echo e(wp_trim_words($desc, 30, '...')); ?>
+
+                            </p>
+                            
+                            <div>
+                                <a href="<?php echo e(get_permalink($caso->ID)); ?>" class="inline-block hover:bg-[#767CB5] bg-[#6A6B75] text-white px-4 py-2 rounded transition">
+                                    Leer caso
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endif; ?>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        <?php if($cuenta == 0): ?> 
+            <div class="h-responsive border-0 rounded-[15px] flex flex-col justify-center gap-2 mb-6 ml-6">
+                No perteneces a ningún caso
+            </div>  
+        <?php endif; ?>
+    </section>
+<?php elseif($tipo == 'events'): ?>
+    <section>
+        <div class="flex items-center space-x-2 mt-5 mb-5">
+            <h3 class="font-bold text-[45px] ml-8">Eventos</h3>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
+            </svg>  
+        </div>
+        <?php
+            $cuenta = 0;
+        ?>
+        <?php $__currentLoopData = $eventos->posts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $evento): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+            <?php
+                $fecha_start = get_field('fecha-inicio', $evento->ID);
+                $fecha_end = get_field('fecha-fin', $evento->ID);
+
+                $caso_relacionado = get_field('caso-relacionado', $evento->ID);
+                $caso_relacionado = $caso_relacionado[0];
+
+                $tipo_caso_id = get_field('type', $caso_relacionado->ID);
+                $image_url = $tipo_caso_id ? get_field('category-pic', 'term_' . $tipo_caso_id) : null;
+
+                $client_id_ev = get_field('client', $caso_relacionado->ID);
+                $client_user_ev = get_userdata($client_id_ev);
+                $client_name_ev = $client_user_ev ? $client_user_ev->display_name : 'Cliente desconocido';
+
+                $lawyer_id_ev = get_field('lawyer', $caso_relacionado->ID);
+                $lawyer_user_ev = get_userdata($lawyer_id_ev);
+                $lawyer_name_ev = $lawyer_user_ev ? $lawyer_user_ev->display_name : 'Abogado desconocido';
             ?>
             <?php if($client_id == $user_id || $lawyer_id == $user_id ||(current_user_can('administrator'))): ?>
                 <?php
--- app/public/wp-content/themes/bufete-theme/resources/views/partials/organisms/organism-lists.blade.php
+++ app/public/wp-content/themes/bufete-theme/resources/views/partials/organisms/organism-lists.blade.php
@@ -9,6 +9,11 @@
         'post_type' => 'caso',
     ];
     $query = new WP_Query($args);
+
+    $argsEventos = [
+        'post_type' => 'evento',
+    ];
+    $eventos = new WP_Query($argsEventos);
 @endphp
 
 @if($tipo == 'cases')
@@ -86,6 +91,79 @@
         @if ($cuenta == 0) 
             <div class="h-responsive border-0 rounded-[15px] flex flex-col justify-center gap-2 mb-6 ml-6">
                 No perteneces a ningún caso
+            </div>  
+        @endif
+    </section>
+@elseif($tipo == 'events')
+    <section>
+        <div class="flex items-center space-x-2 mt-5 mb-5">
+            <h3 class="font-bold text-[45px] ml-8">Eventos</h3>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
+            </svg>  
+        </div>
+        @php
+            $cuenta = 0;
+        @endphp
+        @foreach ($eventos->posts as $evento)
+            @php
+                $fecha_start = get_field('fecha-inicio', $evento->ID);
+                $fecha_end = get_field('fecha-fin', $evento->ID);
+
+                $caso_relacionado = get_field('caso-relacionado', $evento->ID);
+                $caso_relacionado = $caso_relacionado[0];
+
+                $tipo_caso_id = get_field('type', $caso_relacionado->ID);
+                $image_url = $tipo_caso_id ? get_field('category-pic', 'term_' . $tipo_caso_id) : null;
+
+                $client_id_ev = get_field('client', $caso_relacionado->ID);
+                $client_user_ev = get_userdata($client_id_ev);
+                $client_name_ev = $client_user_ev ? $client_user_ev->display_name : 'Cliente desconocido';
+                $client_image_ev = get_field('profile-image', 'user_' . $client_id_ev);
+        
+                $lawyer_id_ev = get_field('lawyer', $caso_relacionado->ID);
+                $lawyer_user_ev = get_userdata($lawyer_id_ev);
+                $lawyer_name_ev = $lawyer_user_ev ? $lawyer_user_ev->display_name : 'Abogado desconocido';
+                $lawyer_image_ev = get_field('profile-image', 'user_' . $lawyer_id_ev);
+            @endphp
+
+
+            @if($client_id_ev == $user_id || $lawyer_id_ev == $user_id ||(current_user_can('administrator')))
+                @php
+                    $cuenta++;
+                @endphp
+                <div class="border border-gray-400 py-5 px-10">
+                    <div class="mb-7 grid grid-cols-5 max-lg:grid-cols-4 gap-4">
+                        <div class="max-lg:col-span-4 col-span-1 flex items-center justify-center">
+                            <img src="{{ $image_url }}" alt="Imagen tipo caso" class="h-[150px] border-0 rounded-[10px] object-cover">
+                        </div>
+                        <div class="col-span-4">
+                            {{-- Título del caso --}}
+                            <h1 class="mb-2 text-[30px] font-bold">{{ $evento->post_title }}</h1>
+                            {{-- Abogado y cliente --}}
+                            <div class="flex flex-row items-center gap-4 mb-3">
+                                <p><b>Caso: </b> {{ get_the_title($caso_relacionado) }}</p>
+                            </div>
+                            <div class="flex flex-row items-center gap-4 mb-3">
+                                <p><b>Fecha inicio: </b>{{ $fecha_start }}</p>
+                            </div>
+                            <div class="flex flex-row items-center gap-4 mb-3">
+                                <p><b>Fecha fin: </b>{{ $fecha_end }}</p>
+                            </div>
+                            {{-- Botón --}}
+                            <div>
+                                <a href="{{ get_permalink($evento->ID) }}" class="inline-block hover:bg-[#767CB5] bg-[#6A6B75] text-white px-4 py-2 rounded transition">
+                                    Leer caso
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endif
+        @endforeach
+        @if ($cuenta == 0) 
+            <div class="h-responsive border-0 rounded-[15px] flex flex-col justify-center gap-2 mb-6 ml-6">
+                No tienes ningún evento programado
             </div>  
         @endif
     </section>
